b094b41661855e425b423c80a1f350b1b0d10e8e
Seonghong Min <sh.min@sk.com>
2022-12-21 14:54:15 +0900

    add local sample

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # besu_local_test
+
+# besu build and run process
+1. install jdk11
+2. git clone https://github.com/hyperledger/besu.git -> {{besu source}} : 다운받은 소스코드 경로 
+3. {{besu_local_test}}/local4nodes 설정 파일 확인 (genesis_IBFT2.json)
+4. {{besu_local_test}}/local4nodes/node{x} 폴더 안의 conf{x}.toml 파일의 경로 수정 -> {{besu_local_test}}의 경로로 수정
+5. vi runAllNodes.new.sh (or runAllNodes.sh) 파일 확인 및 수정 -> {{besu source}}의 경로로 수정 
+6. sh runAllNodes.new.sh (or runAllNodes.sh)
+
+# besu run only process
+1. install jdk11
+2. 로컬에 besu 설치(https://besu.hyperledger.org/en/21.10.9/HowTo/Get-Started/Installation-Options/Install-Binaries/)
+3. {{besu_local_test}}/local4nodes 설정 파일 확인 (genesis_IBFT2.json)
+4. {{besu_local_test}}/local4nodes/node{x} 폴더 안의 conf{x}.toml 파일의 경로 수정 -> {{besu_local_test}}의 경로로 수정
+5. runAllNodes.new.sh (or runAllNodes.sh) 파일 확인 및 수정 -> 24~28라인은 주석처리하고, 30라인을 besuPath=besu로 수정
+6. sh runAllNodes.new.sh (or runAllNodes.sh)
---
+++ local4nodes/runAllNodes.new.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# node 1,2를 background에서 실행하고, node 3을 foreground에서 실행
+# node 1.2는 다른 스크립트로 중지??
+
+clear
+
+workingDir=$(pwd)
+echo $workingDir
+
+echo 'remove old datas'
+rm -rf ./node01/data/*
+rm -rf ./node02/data/*
+rm -rf ./node03/data/*
+rm -rf ./node04/data/*
+
+# 자바 버전 셋팅
+export JAVA_HOME=$(/usr/libexec/java_home -v 11)
+
+# 실행할 besu 소스코드 위치 
+#besuSourcePath=/Users/min/Downloads/besu-git-ref/besu
+besuSourcePath=/Users/min/Downloads/gitlab/besu-client
+#besuSourcePath=/Users/min/Downloads/github-utils/besu-client-tmp
+
+# besu 소스코드를 빌드할 필요가 있을 경우
+cd $besuSourcePath
+./gradlew installDist
+cd $workingDir
+
+# 실행할 besu 바이너리 위치 
+besuPath=$besuSourcePath/build/install/besu/bin/besu
+
+echo '\n* Exec besu --version'
+$besuPath --version
+exitCode=$?
+if [ $exitCode -ne 0 ] ; then
+    echo 'exit by fail ('$besuPath' --version -> exitCode:'$exitCode')'
+    exit 1
+fi
+
+echo "$(date +%Y)년   $(date +%m)월 $(date +%d)일 $(date +%H) 시  $(date +%M) 분  $(date +%S) 초" 
+sleep 3
+
+echo '\n* node4 Exec besu --config-file xxx'
+nohup $besuPath --config-file="./node04/conf4.toml" > node04.log 2>&1 &
+
+echo '\n* node3 Exec besu --config-file xxx'
+nohup $besuPath --config-file="./node03/conf3.toml" > node03.log 2>&1 &
+
+echo '\n* node2 Exec besu --config-file xxx'
+nohup $besuPath --config-file="./node02/conf2.toml" > node02.log 2>&1 &
+
+echo '\n* node1 Exec besu --config-file xxx'
+$besuPath --config-file="./node01/conf1.toml"
