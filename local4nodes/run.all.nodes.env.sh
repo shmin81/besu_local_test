9eace0db7af38c20650616bef8107ec8ea2adca6
Seonghong Min <sh.min@sk.com>
2023-07-27 17:53:56 +0900

    update besu configs

--- local4nodes/run.all.nodes.env.sh
+++ local4nodes/run.all.nodes.env.sh
@@ -20,15 +20,16 @@ export BESU_OPTS=-Xmx1g
 
 # config 파일보다 환경변수로 설정된 값이 우선하여 들어감
 export BESU_TX_POOL_TX_INFLOW_CONTROL_ENABLED=true
-export BESU_TX_POOL_TX_INFLOW_MAX_SIZE=100
+export BESU_TX_POOL_TX_INFLOW_MAX_SIZE=1000
 export BESU_TX_POOL_RETENTION_MINUTES=2
 export BESU_TX_POOL_ENTERPRISE_PENDING_TX_SORTER_ENABLED=true
 export BESU_TX_POOL_TX_FAST_VERIFY_SIGNATURE_ENABLED=true
 export BESU_PERF_ELIMINATE_REDUNDANT_BLOCK_VALIDATION_ENABLED=true
 #######################################
-export BESU_TX_POOL_MAX_SIZE=100
+export BESU_AUTO_LOG_BLOOM_CACHING_ENABLED=false
+export BESU_TX_POOL_MAX_SIZE=1000
 export BESU_TX_POOL_LIMIT_BY_ACCOUNT_PERCENTAGE=1.0
-export BESU_RPC_HTTP_API=ETH,NET,WEB3,TXPOOL
+#export BESU_RPC_HTTP_API=ETH,NET,WEB3,TXPOOL
 #export BESU_MINER_ENABLED=true # Error: Unable to mine without a valid coinbase. Either disable mining (remove --miner-enabled) or specify the beneficiary of mining (via --miner-coinbase <Address>)
 #export BESU_CONFIG_FILE='./node01/conf1.toml' # 이렇게 사용하는 것도 가능함.
 # 실행할 besu 소스코드 위치 
