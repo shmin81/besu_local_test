9d818348025e3b10af1c214e2e446d6dc967f454
Seonghong Min <sh.min@sk.com>
2023-10-17 17:05:17 +0900

    update

--- stopAllNodes.sh
+++ stopAllNodes.sh
@@ -13,6 +13,6 @@ echo 'kill... besu process'
 
 ps -ef | grep "org.hyperledger.besu.Besu --config-file=" | awk '{print $2}' | xargs kill
 
-sleep 1
+sleep 2
 echo '====  status  ===='
 ps -ef | grep "org.hyperledger.besu.Besu"
