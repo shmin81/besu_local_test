7b4b5da53aff408b3e54ae7cb381db514e3bb652
Seonghong Min <sh.min@sk.com>
2024-02-02 16:24:10 +0900

    add clear data script

---
+++ qbft4nodes/clear.Ledger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# node 2,3,4는 background에서 실행하고, node 1을 foreground에서 실행
+# node 2,3,4는 다른 스크립트(stopAllNodes.sh)를 이용해 중지 가능
+
+clear
+
+workingDir=$(pwd)
+echo $workingDir
+
+echo 'remove old datas'
+rm -rf ./node01/data/*
+rm -rf ./node02/data/*
+rm -rf ./node03/data/*
+rm -rf ./node04/data/*
