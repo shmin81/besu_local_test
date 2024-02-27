918717fbcf71ef16a29f7b453020d84547de864f
Seonghong Min <sh.min@sk.com>
2024-02-27 11:05:23 +0900

    update script

--- qbft4nodes/_singleRun/1.4.chainz.node.run.sh
+++ qbft4nodes/_singleRun/1.4.chainz.node.run.sh
@@ -7,6 +7,7 @@ echo "$(date +%Y)년 $(date +%m)월 $(date +%d)일  $(date +%H)시 $(date +%M)�
 # rm -rf ./node04/data/*
 
 export BESU_IDENTITY="val04"
+export BESU_BOOTNODES=enode://d3045c8ef00ca882f4acfd2d4f76479d18245c2d6a6d7ad5a3047591919d94c114470863a9aa9e3fb6a93eca7569cfe143dcf321bfdbba419323ec305dcf6907@127.0.0.1:30303
 
 # besuSourcePath=$workingDir/../../besu-binary/23.1.3.2
 # #besuPath=$besuSourcePath/build/install/besu/bin/besu
